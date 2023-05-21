3303289180e3df34da1612d4eb232bf16e841b6d
hessennasser <hessenaloosh@gmail.com>
2023-05-21 23:17:23 +0300

    All New UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '3rem',
+
+      }
+    },
+
   },
   plugins: [],
 }
